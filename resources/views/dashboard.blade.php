5128bbcda1c0e1b962886221ca85c0b4650faa97
MD Saifur Rahaman <asifforizy@gmail.com>
2026-04-23 19:59:27 +0600

    clear readme file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('User Dashboard') }}
-        </h2>
+        <div class="flex ">
+            <a  href="{{ route('viewallproducts') }}"
+                class="font-semibold  text-xl text-gray-800 dark:text-gray-200 leading-tight">← Back to Shop</a>
+
+        </div>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
                     @yield('view_orders')
